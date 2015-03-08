cb8e1c5814ffb42d43388191b5961721a56062cf
vineeshvs <vineeshvsjobs@gmail.com>
2015-03-08 23:48:08 +0530

    The second commit

---
+++ git_commit.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+git init
+git pull https://github.com/vineeshvs/research.git
+git add *
+echo "\nEnter the commit message"
+echo "*************************\n"
+git commit -m "$1"
+echo "\nBelow is the git status"
+echo "*************************\n"
+git status
+echo "\nPushing to the directory  'https://github.com/vineeshvs/research.git'"
+echo "***********************************************************************" 
+git push https://github.com/vineeshvs/research.git
